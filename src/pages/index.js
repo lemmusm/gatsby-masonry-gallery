bcdeb0b9eaa1f4bf9d5cbcb1dae0eade3465d663
lemmusm <lemmusm@gmail.com>
2020-11-10 17:30:02 -0600

    Add style and structure gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import Gallery from "../components/gallery.js"
+import "../assets/css/global.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = ({ data }) => {
+  const images = data.allImageSharp.edges
+  return <Gallery imgs={images} />
 }
+
+export const query = graphql`
+  query {
+    allImageSharp {
+      edges {
+        node {
+          fluid(maxWidth: 900) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Home
