e3e3f36ced09dc8a83696d782792b15fcba0fe61
lemmusm <lemmusm@gmail.com>
2020-11-10 14:38:33 -0600

    Add dependencies and edit gatsby-config.js
    	Dependencies: gatsby-image, gatsby-plugin-sharp, gatsby-transformer-sharp, gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+const path = require(`path`)
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `assets`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
